ce37f3493a830329855dab20f4352cb3736e5ddc
killer13 <hnlylyb@gmail.com>
2025-01-08 00:02:13 +0800

    add log.

--- src/worker.mjs
+++ src/worker.mjs
@@ -4,7 +4,10 @@ export default {
     let targetURL = new URL('https://generativelanguage.googleapis.com')
   
     targetURL.pathname = url.pathname
+    console.log(targetURL.pathname)
     targetURL.search = url.search
+    console.log(targetURL.search)
+
   
     let newRequest = new Request(targetURL, {
       method: request.method,
